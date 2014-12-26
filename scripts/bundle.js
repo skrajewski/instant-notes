f517aa2aea69bdef5cba7ad47b1cf51006cbd6b6
Szymon Krajewski <szykra@gmail.com>
2014-12-26 20:28:22 +0100

    Fix indentation

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,56 +1,56 @@
 var markdown = require('markdown').markdown,
-	base64 = require('js-base64').Base64,
-	editor = document.querySelector('#editor'),
-	preview = document.querySelector('#preview'),
-	divider = document.querySelector('#divider'),
-	title = document.querySelector('title');
+    base64 = require('js-base64').Base64,
+    editor = document.querySelector('#editor'),
+    preview = document.querySelector('#preview'),
+    divider = document.querySelector('#divider'),
+    title = document.querySelector('title');
 
 function showInPreview(content) {
-	window.history.replaceState({}, null, '#/' + base64.encode(content));
+    window.history.replaceState({}, null, '#/' + base64.encode(content));
 
-	preview.innerHTML = markdown.toHTML(content);
-	title.innerHTML = content.split('\n')[0];
+    preview.innerHTML = markdown.toHTML(content);
+    title.innerHTML = content.split('\n')[0];
 }
 
 function refresh() {
-	showInPreview(editor.value);
+    showInPreview(editor.value);
 }
 
 divider.addEventListener('mousedown', function(e) {
-	var movingDivider = function(ex) {
-		var x = e.clientX,
-			widthEditor;
+    var movingDivider = function(ex) {
+        var x = e.clientX,
+            widthEditor;
 
-		widthEditor = parseFloat(ex.clientX / window.innerWidth * 100).toFixed(8);
+        widthEditor = parseFloat(ex.clientX / window.innerWidth * 100).toFixed(8);
 
-		if(widthEditor < 20) widthEditor = 20;
-		else if(widthEditor > 80) widthEditor = 80;
+        if(widthEditor < 20) widthEditor = 20;
+        else if(widthEditor > 80) widthEditor = 80;
 
-		editor.style.width = "calc("+ widthEditor + "% - 3px)";
-		preview.style.width = "calc("+ (100 - widthEditor) + "% - 3px)";
+        editor.style.width = "calc("+ widthEditor + "% - 3px)";
+        preview.style.width = "calc("+ (100 - widthEditor) + "% - 3px)";
 
-		event.preventDefault();  
-	};
+        event.preventDefault();  
+    };
 
-	window.addEventListener('mousemove', movingDivider);
+    window.addEventListener('mousemove', movingDivider);
 
-	window.addEventListener('mouseup', function(e) {
-		window.removeEventListener('mousemove', movingDivider);
-	});
+    window.addEventListener('mouseup', function(e) {
+        window.removeEventListener('mousemove', movingDivider);
+    });
 });
 
 editor.addEventListener('keyup', function(e) {
-	refresh();
+    refresh();
 });
 
 window.onload = function() {
-	var data = window.location.hash;
+    var data = window.location.hash;
 
-	if(!data) return;
+    if(!data) return;
 
-	data = data.slice(2, data.length);
-	editor.value = base64.decode(data);
+    data = data.slice(2, data.length);
+    editor.value = base64.decode(data);
 
-	refresh();
+    refresh();
 };
 
